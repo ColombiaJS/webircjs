177523edf951e35f9eb2ec271ebcc131d17bb7b9
Julian Duque <julianduquej@gmail.com>
2013-01-12 21:00:37 -0500

    add support to private messages

--- public/javascripts/webirc.js
+++ public/javascripts/webirc.js
@@ -1,9 +1,10 @@
 ;(function ($, window, undefined) {
   'use strict';
-  var currentChannel = null;
+  var currentTarget = null;
   var channels = {};
-  var privates = [];
+  var privates = {};
   var nickname = null;
+  var isUser = false;
 
   $(document).ready(function() {
     $('#login').reveal();
@@ -30,28 +31,16 @@
       socket.on('join', function(channel, nick, message) {
         if(nick === nickname) {
           channels[channel] = {};
-          currentChannel = channel;
 
           // Add tab
-          var tabSource = $('#tab-template').html();
-          var tabTemplate = Handlebars.compile(tabSource);
-          var tab = tabTemplate({ target: channel });
-
-          $('#tabs').append(tab);
-          activateTabs();
+          createTab(channel);
 
           // Add window
-          var winSource = $('#window-template').html();
-          var winTemplate = Handlebars.compile(winSource);
-          channel = channel.replace('#', '');
-          var win = winTemplate({ target: channel });
-
-          $('#windows').append(win);
+          createWindow(channel);
         }
       });
 
       socket.on('topic', function(channel, topic, nick, message) {
-        //console.log(channel, topic, nick, message);
         if(channels[channel]) {
           channels[channel].topic = topic;
         }
@@ -63,19 +52,19 @@
         }
       });
 
-      socket.on('message', function(from, to, text, message) {
-        if(channels[to]) {
-          console.log(message);
-          if(~to.indexOf('#')) {
-
-            var msgSource = $('#message-template').html();
-            var msgTemplate = Handlebars.compile(msgSource);
-            var msg = msgTemplate({ nickname: from, message: text });
-
-            $(to).append(msg);
-            $(to).get(0).scrollTop = 10000000;
-          }
+      socket.on('message', function(from, to, text, msg) {
+        if(~to.indexOf('#') && channels[to]) {
+          message(from, to, text);
         }
+      });
+
+      socket.on('pm', function(from, text, msg) {
+          if(!privates[from]) {
+            createTab(from);
+            createWindow(from);
+            privates[from] = { nick: from };
+          }
+          message(from, '#' + from , text);
       });
 
       socket.on('info', function(info) {
@@ -86,17 +75,17 @@
       $('#message-text').keydown(function(e) {
         var code = (e.keyCode ? e.keyCode : e.which);
         if(code == 13) {
-          var message = $(this).val();
-          socket.emit('say', currentChannel, message);
-          $(this).val('');
-
+          var msg = $(this).val();
+          var target = currentTarget;
           // Add message
-          var msgSource = $('#message-template').html();
-          var msgTemplate = Handlebars.compile(msgSource);
-          var msg = msgTemplate({ nickname: nickname, message: message });
+          message(nickname, target, msg);
 
-          $(currentChannel).append(msg);
-          $(currentChannel).get(0).scrollTop = 10000000;
+          if(isUser) {
+            target = target.replace('#', '');
+          }
+
+          socket.emit('say', target, msg);
+          $(this).val('');
         }
       });
     });
@@ -104,6 +93,33 @@
     activateTabs();
 
   });
+
+  function createTab(target) {
+    var tabSource = $('#tab-template').html();
+    var tabTemplate = Handlebars.compile(tabSource);
+    var tab = tabTemplate({ target: target });
+
+    $('#tabs').append(tab);
+    activateTabs();
+  }
+
+  function createWindow(target) {
+    var winSource = $('#window-template').html();
+    var winTemplate = Handlebars.compile(winSource);
+    target = target.replace('#', '');
+    var win = winTemplate({ target: target });
+
+    $('#windows').append(win);
+  }
+
+  function message(from, to, msg) {
+    var msgSource = $('#message-template').html();
+    var msgTemplate = Handlebars.compile(msgSource);
+    var msg = msgTemplate({ nickname: from, message: msg });
+
+    $(to).append(msg);
+    $(to).get(0).scrollTop = 10000000;
+  }
 
   function activateTabs() {
     $('.tab').unbind('click');
@@ -126,13 +142,18 @@
         for(var nick in nicks) {
           $('#users').append('<p>' + nicks[nick] + nick + '</p>');
         }
-
+        currentTarget = target;
+        isUser = false;
         $('#users').show();
       } else if(target == 'status') {
         $('#status').show();
         $('#users').hide();
+        isUser = false;
       } else {
-        $('#nick_' + target).show();
+        $('#' + target).show();
+        $('#users').hide();
+        currentTarget = '#' + target;
+        isUser = true;
       }
 
     });
